2ddc003df9157b92339de176fce76b9f6bb779f7
Nabeel Hamad <nabeel.hamad@lloydsbanking.com>
2021-11-30 16:28:02 +0000

    create legba triggers

--- main.tf
+++ main.tf
@@ -39,3 +39,48 @@ resource "google_cloudbuild_trigger" "tst-push-api_supporting_infra_apply" {
   filename       = "pipeline/ci/cloudbuild-apply.yaml"
   disabled       = false
 }
+
+
+
+
+####################################################################################
+# LEGBA trigger
+####################################################################################
+
+# PLAN
+
+resource "google_cloudbuild_trigger" "tst-push-api_supporting_infra_plan" {
+  provider = google-beta
+  name = "Plan-dcsgva-legba-tst"
+
+  github {
+    owner = "gva-nhamad"
+    name = "api-receiver-legba"
+    pull_request {
+      branch = "^main$"
+    }
+  }
+
+  description    = "PLAN: automated trigger for creating the push api supporting infra"
+  filename       = "cloudbuild.yaml"
+  disabled       = false
+}
+
+# APPLY
+
+resource "google_cloudbuild_trigger" "tst-legba_supporting_infra_apply" {
+  provider = google-beta
+  name = "Apply-dcsgva-legba-tst"
+
+  github {
+    owner = "gva-nhamad"
+    name = "api-receiver-legba"
+    push {
+      branch = "main"
+    }
+  }
+
+  description    = "APPLY: automated trigger for creating the push api supporting infra"
+  filename       = "cloudbuild.yaml"
+  disabled       = false
+}
